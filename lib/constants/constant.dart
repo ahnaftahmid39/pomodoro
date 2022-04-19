097270af27fb122baaddfc31df438e6ab9a1c3b1
Ahnaf <tahmidtahmid31@gmail.com>
2022-04-19 22:03:21 +0600

    defined some color for dark theme

--- lib/constants/constant.dart
+++ lib/constants/constant.dart
@@ -12,3 +12,15 @@ const kTextClr = Color(0xFF7C0000);
 const kTextClr2 = Color(0xFF851616);
 const kBtnBgClr = kBgClr2;
 const kClkBgClr = kBgClr3;
+
+// for dark theme:
+
+const kBgClrDark = Color.fromRGBO(6, 4, 32, 90);
+const kBgClrNoOpDark = Color(0xFF1F1D36);
+const kBgClr2Dark = Color(0xFF3F3351);
+const kBgClr3Dark = Color(0xFFE9A6A6);
+
+const kTextClrDark = kBgClr4;
+const kTextClr2Dark = Color(0xFFF8F8F8);
+const kBtnBgClrDark = kBgClr2Dark;
+const kClkBgClrDark = kBgClr3Dark;
